e4bfc888739098f32d2c15de05922542f131dbbd
liuguirong <liuguirong@kingsoft.com>
2021-01-01 13:17:04 +0800

    myself contract address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x08aff414babf19efdd7c84e53984aea873e088bf'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
